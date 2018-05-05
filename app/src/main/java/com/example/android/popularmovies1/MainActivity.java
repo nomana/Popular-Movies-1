8fe98a1ceebf2e77e3760c9a1295ba12b1ed6897
joshuak <joshuakagawa@gmail.com>
2018-05-05 13:57:20 -0400

    Latest version

--- app/src/main/java/com/example/android/popularmovies1/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies1/MainActivity.java
@@ -1,16 +1,115 @@
 package com.example.android.popularmovies1;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.android.popularmovies1.utilities.NetworkUtils;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mSearchTextView;
+
+    private TextView mUrlDisplayTextView;
+
+    private TextView mUrlResultsTextView;
+
+    private TextView mTopResult;
+
+    private String jsonFromUrl;
+
+    final static String API_KEY = "ca1eda3d7d2727738ebbeffcde814ed8";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSearchTextView = (TextView) findViewById(R.id.search_action);
+        mUrlDisplayTextView = (TextView) findViewById(R.id.url_display);
+        mUrlResultsTextView = (TextView) findViewById(R.id.url_results);
+        mTopResult = (TextView) findViewById(R.id.first);
+
     }
 
+    private void makeUrlQuery() {
+        URL popularUrl = NetworkUtils.buildPopularUrl(API_KEY);
+        URL topRatedUrl = NetworkUtils.buildTopRatedUrl(API_KEY);
+        mUrlDisplayTextView.setText(popularUrl.toString());
+
+        new UrlQueryTask().execute(popularUrl);
+
+    }
+
+    private void closeOnError() {
+        finish();
+        Toast.makeText(this, R.string.main_error_message, Toast.LENGTH_SHORT).show();
+    }
+
+    public class UrlQueryTask extends AsyncTask<URL, Void, String> {
+        @Override
+        protected String doInBackground(URL... params) {
+            URL searchUrl = params[0];
+
+            String urlResults = null;
+            try {
+                urlResults = NetworkUtils.getResponseFromHttpUrl(searchUrl);
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return urlResults;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if (s != null && !s.equals("")) {
+                mUrlResultsTextView.setText(s);
+                jsonFromUrl = s;
+                try {
+                    JSONObject json = new JSONObject(jsonFromUrl);
+                    JSONArray results = json.getJSONArray("results");
+                    List<String> first = new ArrayList<>();
+                    first.add(results.getString(0));
+                    String firstString = android.text.TextUtils.join(", ", first);
+                    mTopResult.setText(firstString);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemThatWasClickedId = item.getItemId();
+        if (itemThatWasClickedId == R.id.action_sort) {
+            makeUrlQuery();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     //API Key (v3 auth)
     //ca1eda3d7d2727738ebbeffcde814ed8
---
+++ app/src/main/java/com/example/android/popularmovies1/utilities/NetworkUtils.java
@@ -0,0 +1,79 @@
+package com.example.android.popularmovies1.utilities;
+
+import android.net.Uri;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+/**
+ * Created by Joshua on 5/5/2018.
+ */
+
+public class NetworkUtils {
+
+    final static String MOVIE_DB_URL = "https://api.themoviedb.org/3/movie";
+
+    final static String POPULAR_PATH = "popular";
+
+    final static String TOP_RATED_PATH = "top_rated";
+
+    final static String PARAM_API_KEY = "api_key";
+
+    //Build URL to query for popular movies
+    public static URL buildPopularUrl(String apiKey) {
+        Uri builtUri = Uri.parse(MOVIE_DB_URL).buildUpon()
+                .appendPath(POPULAR_PATH)
+                .appendQueryParameter(PARAM_API_KEY, apiKey)
+                .build();
+
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return url;
+    }
+
+    //Build URL to query for top rated movies
+    public static URL buildTopRatedUrl(String apiKey) {
+        Uri builtUri = Uri.parse(MOVIE_DB_URL).buildUpon()
+                .appendPath(TOP_RATED_PATH)
+                .appendQueryParameter(PARAM_API_KEY, apiKey)
+                .build();
+
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return url;
+    }
+
+    //Search internet using the URL
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+}
